fa810037d57b26f23f982f9d20afb852e93a405c
Akansh Saxena <akanshsaxena@Mac.lan>
2026-04-26 10:22:08 +0530

    log changes to warn

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+try {
+  require("newrelic");
+  console.log(JSON.stringify({ severity: "INFO", message: "New Relic agent loaded" }));
+} catch (err: any) {
+  console.log(JSON.stringify({ severity: "ERROR", message: `New Relic failed to load: ${err.message}` }));
+}
+
 import { createApp } from "./app";
 import { env } from "./config/env";
 import { connectDB } from "./config/db";
 
 const startServer = async () => {
-  console.warn("NEW_RELIC_LICENSE_KEY set:", !!process.env.NEW_RELIC_LICENSE_KEY);
-  console.warn("NEW_RELIC_APP_NAME:", process.env.NEW_RELIC_APP_NAME);
-  console.warn("NEW_RELIC_HOME:", process.env.NEW_RELIC_HOME);
+  console.log(JSON.stringify({ severity: "INFO", message: `NEW_RELIC_LICENSE_KEY set: ${!!process.env.NEW_RELIC_LICENSE_KEY}` }));
+  console.log(JSON.stringify({ severity: "INFO", message: `NEW_RELIC_APP_NAME: ${process.env.NEW_RELIC_APP_NAME}` }));
+  console.log(JSON.stringify({ severity: "INFO", message: `NEW_RELIC_HOME: ${process.env.NEW_RELIC_HOME}` }));
 
   await connectDB();
 
